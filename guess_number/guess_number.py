49e7159629c9ae69c0e15383925d6d82bdc0786e
Marina <manyak76@list.ru>
2022-06-06 14:45:19 +0300

    master

---
+++ guess_number/guess_number.py
@@ -0,0 +1,58 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Угадываем число по алгоритму
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    low_bound = 1 # нижняя граница для random
+    upper_bound = 101 # верхняя граница для random
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(low_bound, upper_bound)
+        
+        if number == predict_number:
+            break
+        elif number > predict_number:
+            low_bound = predict_number + 1 # переопределяем нижнюю границу для random
+        else:
+            upper_bound = predict_number # переопределяем верхнюю границу для random
+            
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
